1c71478c262ff0d4e767193e8a7d086dd69e87f2
greatness2022 <miracleonyenwe2586@gmail.com>
2024-01-25 14:15:36 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
 
@@ -7,6 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
@@ -15,6 +17,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
